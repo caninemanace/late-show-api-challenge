73834822870e6feeea15f104c914b5132f36460e
Eddie Njoroge <eddie.njoroge2306@gmail.com>
2025-06-23 11:48:20 +0300

    database initalized together with all migrations

--- server/models/user.py
+++ server/models/user.py
@@ -6,8 +6,8 @@ class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
-    
+    password_hash = db.Column(db.String(256), nullable=False)
+
     def __init__(self, username, password):
         self.username = username
         self.password_hash = generate_password_hash(password)
